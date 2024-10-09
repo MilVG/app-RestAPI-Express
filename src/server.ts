9e8ebdbaca6ffe6a8275ffd1d36fb72217479dff
notlim_dev <elmotigre6@gmail.com>
2024-10-09 10:59:30 -0500

    0011: Definición de Métodos Http.

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,23 @@ const server = express()
 
 //Routing
 server.get('/', (req, res) => {
-  const auth = true
-  res.send(auth)
+  res.json('Desde GET')
+})
+
+server.post('/', (req, res) => {
+  res.json('Desde POST')
+})
+
+server.put('/', (req, res) => {
+  res.json('Desde PUT')
+})
+
+server.patch('/', (req, res) => {
+  res.json('Desde PATCH')
+})
+
+server.delete('/', (req, res) => {
+  res.json('Desde DELETE')
 })
 
 export default server
