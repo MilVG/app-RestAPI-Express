22ee450136062471cba40286c68977465144b4d0
notlim_dev <elmotigre6@gmail.com>
2024-10-09 14:40:35 -0500

    Cambios Finalziados de Conexion a base de datos Servidor

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ server.listen(4000, () => {
   console.log(`REST API en el puerto 4000`);
 
 })
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import express from "express"
-
+import router from "./router"
+import db from "./config/db"
 const server = express()
 
-//Routing
-server.get('/', (req, res) => {
-  const auth = true
-  res.send(auth)
-})
+async function conectionDB() {
+  try {
+
+    await db.sync()
+    console.log('conexione xitossa');
+
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+
+conectionDB()
+server.use('/api/products', router)
 
 export default server
 
