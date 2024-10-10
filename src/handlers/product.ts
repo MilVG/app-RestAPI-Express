3845acf74bd857eefdd0580686a4ac3e07cece7a
notlim_dev <elmotigre6@gmail.com>
2024-10-10 16:03:45 -0500

    Validacion  campos vacios strings - express-validator

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,6 +1,17 @@
 import { Request, Response } from "express"
 import Product from "../models/Product.model"
+import { check, validationResult } from "express-validator"
 export const createProduct = async (req: Request, res: Response) => {
+
+  //validacion
+  await check('name').notEmpty().withMessage('El nombre de Producto no puede ir vacio').run(req)
+
+  let errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+
   const product = await Product.create(req.body)
   res.json({ data: product })
 }
