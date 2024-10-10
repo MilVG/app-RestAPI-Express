563cfecf32273dd633c39a2107c13a83aeed94d1
notlim_dev <elmotigre6@gmail.com>
2024-10-10 17:24:07 -0500

    Eliminación codigo validación del handler

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,17 +1,9 @@
 import { Request, Response } from "express"
 import Product from "../models/Product.model"
-import { check, validationResult } from "express-validator"
+import { validationResult } from "express-validator"
 
 
 const createProduct = async (req: Request, res: Response) => {
-
-  //validacion
-  await check('name').notEmpty().withMessage('El nombre de Producto no puede ir vacio').run(req)
-  await check('price')
-    .isNumeric().withMessage('Valor no válido')
-    .notEmpty().withMessage('el precio del Producto no puede ir vacio')
-    .custom(value => value > 0).withMessage('Precio no válido')
-    .run(req)
 
   let errors = validationResult(req)
   if (!errors.isEmpty()) {
