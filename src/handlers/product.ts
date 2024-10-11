fde7724d3322b9c3c04f3c5d5c006f1f6082eaf6
notlim_dev <elmotigre6@gmail.com>
2024-10-11 04:16:46 -0500

    Implementación handler getProductById
    => para retornar datos mediante la url dinamica utilizamos la propiedad
    por default de paramas para obtener los parametros.
    => para obtener datos mediante un id utilizamos la funcion findByPk(id)
    => validamos si la consulta obtiene datos o no con el id brindado.

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -9,9 +9,24 @@ export const getProducts = async (req: Request, res: Response) => {
       order: [
         ['id', 'DESC']
       ],
-      attributes: { exclude: ['createdAt', 'updatedAt', 'availability'] }
     })
     res.json({ data: products })
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+export const getProductById = async (req: Request, res: Response) => {
+
+  try {
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if (!product) {
+      res.status(404).json({ error: 'Producto no encontrado' })
+      return
+    }
+    res.json({ data: product })
   } catch (error) {
     console.log(error);
 
