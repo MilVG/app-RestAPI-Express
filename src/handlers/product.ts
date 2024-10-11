051c394db9bf86d270f339ed6a199481abdc19c5
notlim_dev <elmotigre6@gmail.com>
2024-10-11 03:51:45 -0500

    Consulta endpoint Metodo GET  handler - getProducts
    => listar datos de la base de datos con la funcion findAll()
    => Ordenar datos con la propiedad order de la Funcion findAll()
    => Limitar datos con la propiedad limit de la funcion findAll()

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,7 +1,23 @@
 import { Request, Response } from "express"
 import Product from "../models/Product.model"
 
-const createProduct = async (req: Request, res: Response) => {
+
+export const getProducts = async (req: Request, res: Response) => {
+
+  try {
+    const products = await Product.findAll({
+      order: [
+        ['id', 'DESC']
+      ],
+      limit: 3
+    })
+    res.json({ data: products })
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+export const createProduct = async (req: Request, res: Response) => {
 
   try {
     const product = await Product.create(req.body)
@@ -12,5 +28,3 @@ const createProduct = async (req: Request, res: Response) => {
 
   }
 }
-
-export default createProduct
