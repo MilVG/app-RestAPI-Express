b407639fb8e7eb185e1571ee406591437b727668
notlim_dev <elmotigre6@gmail.com>
2024-10-11 04:43:18 -0500

    Implementacion handler updateProductPut
    =>El metodo PUT nos permite actulizar compleamente.
    =>Comprobacion si existe el id dinamico y depsues actulizamos.
    => utilizamos la funcion update(req.id) para actualizar.

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -43,3 +43,30 @@ export const createProduct = async (req: Request, res: Response) => {
 
   }
 }
+export const updateProductPut = async (req: Request, res: Response) => {
+
+  try {
+
+    //comprobar que exista
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if (!product) {
+      res.status(404).json({ error: 'Producto no encontrado' })
+      return
+    }
+
+    //actualizar
+    await product.update(req.body)
+    await product.save()
+
+
+    res.json({ data: product })
+
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+
+
