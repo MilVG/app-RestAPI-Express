5788b2dc08876c5e1432c11c99bf275bf69ca673
notlim_dev <elmotigre6@gmail.com>
2024-10-11 23:20:59 -0500

    Pruebas de validacion envio de body vacio
    1º=> segmento comprobar status 400
    2ª=> segmento comporbacion de la propiedad de respuesta errors.
    3º=> segmento comporbar body-errors contiene 4 repsuestas.

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -2,7 +2,18 @@ import request from "supertest";
 import server from "../../server";
 
 describe('POST /api/products', () => {
-  it('shoul create a new product', async () => {
+  it('should display validation errors', async () => {
+    const response = await request(server).post('/api/products').send({})
+    expect(response.status).toBe(400)
+    expect(response.body).toHaveProperty('errors')
+    expect(response.body.errors).toHaveLength(4)
+
+
+    expect(response.status).not.toBe(404)
+    expect(response.body.errors).not.toHaveLength(2)
+  })
+
+  it('should create a new product', async () => {
     const response = await request(server).post('/api/products').send({
       name: "mouse-Testing",
       price: 50
