6d489df0c9df7d87ae014760a2988e2c3ef7b5ea
notlim_dev <elmotigre6@gmail.com>
2024-10-13 14:28:37 -0500

    Resolviendo y Eliminado algunas Lineas de codigo Innecesarias
    => estas lineas eliminadas en nuestro test de metricas nos indican que
    no son utilizadas y recomendable

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -4,114 +4,83 @@ import Product from "../models/Product.model"
 
 export const getProducts = async (req: Request, res: Response) => {
 
-  try {
-    const products = await Product.findAll({
-      order: [
-        ['id', 'DESC']
-      ],
-    })
-    res.json({ data: products })
-  } catch (error) {
-    console.log(error);
-
-  }
+  const products = await Product.findAll({
+    order: [
+      ['id', 'DESC']
+    ],
+  })
+  res.json({ data: products })
 }
 export const getProductById = async (req: Request, res: Response) => {
 
-  try {
-    const { id } = req.params
-    const product = await Product.findByPk(id)
+  const { id } = req.params
+  const product = await Product.findByPk(id)
 
-    if (!product) {
-      res.status(404).json({ error: 'Producto no encontrado' })
-      return
-    }
-    res.json({ data: product })
-  } catch (error) {
-    console.log(error);
-
+  if (!product) {
+    res.status(404).json({ error: 'Producto no encontrado' })
+    return
   }
+  res.json({ data: product })
+
 }
 export const createProduct = async (req: Request, res: Response) => {
 
-  try {
-    const product = await Product.create(req.body)
-    res.status(201).json({ data: product })
+  const product = await Product.create(req.body)
+  res.status(201).json({ data: product })
 
-  } catch (error) {
-    console.log(error);
-
-  }
 }
 export const updateProductPut = async (req: Request, res: Response) => {
 
-  try {
+  //comprobar que exista
+  const { id } = req.params
+  const product = await Product.findByPk(id)
 
-    //comprobar que exista
-    const { id } = req.params
-    const product = await Product.findByPk(id)
-
-    if (!product) {
-      res.status(404).json({ error: 'Producto no encontrado' })
-      return
-    }
-
-    //actualizar
-    await product.update(req.body)
-    await product.save()
-
-
-    res.json({ data: product })
-
-  } catch (error) {
-    console.log(error);
-
+  if (!product) {
+    res.status(404).json({ error: 'Producto no encontrado' })
+    return
   }
+
+  //actualizar
+  await product.update(req.body)
+  await product.save()
+
+
+  res.json({ data: product })
+
+
 }
 
 export const updateProductPatchAvailability = async (req: Request, res: Response) => {
 
-  try {
+  const { id } = req.params
+  const product = await Product.findByPk(id)
 
-    //comprobar que exista
-    const { id } = req.params
-    const product = await Product.findByPk(id)
-
-    if (!product) {
-      res.status(404).json({ error: 'Producto no encontrado' })
-      return
-    }
-
-    //actualizar
-    product.availability = !product.dataValues.availability
-    await product.save()
-
-
-    res.json({ data: product })
-
-  } catch (error) {
-    console.log(error);
-
+  if (!product) {
+    res.status(404).json({ error: 'Producto no encontrado' })
+    return
   }
+
+  //actualizar
+  product.availability = !product.dataValues.availability
+  await product.save()
+
+
+  res.json({ data: product })
+
 }
 export const deleteProduct = async (req: Request, res: Response) => {
 
-  try {
-    //comprobar que exista
-    const { id } = req.params
-    const product = await Product.findByPk(id)
+  //comprobar que exista
+  const { id } = req.params
+  const product = await Product.findByPk(id)
 
-    if (!product) {
-      res.status(404).json({ error: 'Producto no encontrado' })
-      return
-    }
-
-    await product.destroy()
-    res.json({ data: 'Producto Eliminado' })
-
-  } catch (error) {
-    console.log(error);
-
+  if (!product) {
+    res.status(404).json({ error: 'Producto no encontrado' })
+    return
   }
+
+  await product.destroy()
+  res.json({ data: 'Producto Eliminado' })
+
 }
 
