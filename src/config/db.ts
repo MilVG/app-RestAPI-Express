6ffa03bc8315504e82a37ca7054eedfdc6318967
notlim_dev <elmotigre6@gmail.com>
2024-10-18 20:15:21 -0500

    modificacion de extesion ts para archivos del modelo
    => al compilar los archivos seran js por lo tanto traeria errores y
    mescla de archivos por eso se eliminó la extesion y se dejó todos los
    archivos que pertenescan a esa carpeta.

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,7 +8,7 @@ const db = new Sequelize(process.env.NAME_BD, process.env.USER_NAME, process.env
   host: process.env.NAME_HOST,  // O 'localhost' si es en tu máquina local
   dialect: 'postgres',
   port: 5432,
-  models: [__dirname + '/../models/**/*.ts'],
+  models: [__dirname + '/../models/**/*'],
   logging: false
 });
 
